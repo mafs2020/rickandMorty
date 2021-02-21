df09f2484b94db831ac3646e648b3955232db76d
MAFS <martin117h@gmail.com>
2021-02-21 17:16:46 -0600

    se termina el traspaso de info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// shared module
+import { SharedModule } from './shared/shared.module';
+
+import { HttpClientModule } from '@angular/common/http';
+
+// rutas
+import { routerModuleAPP } from './app.routes';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PersonajeComponent } from './components/personaje/personaje.component';
+import { PagesModule } from './pages/pages.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    PersonajeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routerModuleAPP,
+    SharedModule,
+    HttpClientModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
